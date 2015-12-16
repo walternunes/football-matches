6dec5e03626576aede119eb63515532debab3e1b
Walter <jnunes.walter@gmail.com>
2015-12-15 22:15:07 -0200

    Create first menu

--- app/src/main/java/jnuneslab/com/footballmatches/MainActivityFragment.java
+++ app/src/main/java/jnuneslab/com/footballmatches/MainActivityFragment.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    private ScoreAdapter mScoreAdapter;
     public MainActivityFragment() {
     }
 
@@ -18,7 +19,11 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        final ListView score_list = (ListView) rootView.findViewById(R.id.score_list_view);
+        mScoreAdapter = new ScoreAdapter(getActivity());
+        score_list.setAdapter(mScoreAdapter);
 
+         new FetchScoreTask(getContext(), mScoreAdapter).execute();
         return rootView;
     }
 }
---
+++ app/src/main/java/jnuneslab/com/footballmatches/ScoreAdapter.java
@@ -0,0 +1,56 @@
+package jnuneslab.com.footballmatches;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Walter on 23/11/2015.
+ */
+public class ScoreAdapter extends BaseAdapter {
+    public static final int COL_HOME = 3;
+    public static final int COL_AWAY = 4;
+    public static final int COL_HOME_GOALS = 6;
+    public static final int COL_AWAY_GOALS = 7;
+    public static final int COL_DATE = 1;
+    public static final int COL_LEAGUE = 5;
+    public static final int COL_MATCHDAY = 9;
+    public static final int COL_ID = 8;
+    public static final int COL_MATCHTIME = 2;
+    public double detail_match_id = 0;
+    private String FOOTBALL_SCORES_HASHTAG = "#Football_Scores";
+
+    private Context mContext;
+    ScoreAdapter(Context context){
+        mContext = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View mItem = LayoutInflater.from(mContext).inflate(R.layout.scores_list_item, parent, false);
+        //ViewHolder mHolder = new ViewHolder(mItem);
+        //  mItem.setTag(mHolder);
+        return mItem;
+    }
+}
